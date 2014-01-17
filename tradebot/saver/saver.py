ab5442fe39e129784a85ccb5ce8c756c273942a2
Alexey Fansky <alexey.fansky@effective-soft.com>
2014-01-17 17:05:42 +0300

    fixing ticks saving

--- tradebot/saver/saver.py
+++ tradebot/saver/saver.py
@@ -1,20 +1,22 @@
+import threading
 import btcebot
 
 
 class DataSaver(object):
     def __init__(self, database_path):
-        self.db = None
+        self.storage = threading.local()
+        self.storage.db = None
         self.database_path = database_path
 
     def getDB(self):
-        if self.db is None:
-            self.db = btcebot.MarketDatabase(self.database_path)
+        if getattr(self.storage, 'db', None) is None:
+            self.storage.db = btcebot.MarketDatabase(self.database_path)
 
-        return self.db
+        return self.storage.db
 
     def closeDB(self):
-        if self.db is not None:
-            self.db.close()
+        if self.storage.db is not None:
+            self.storage.db.close()
 
     def saveDepth(self, t, pair, asks, bids):
         self.getDB().insertDepth(t, pair, asks, bids)
@@ -24,4 +26,3 @@ class DataSaver(object):
 
     def saveTick(self, t, pair, tick):
         self.getDB().insertTick(t, pair, tick)
-        pass
