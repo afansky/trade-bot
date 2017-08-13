2fc73afcae7832bb1df95a256d4b162fd01905e2
Alexey Fansky <alexey.fansky@effective-soft.com>
2017-08-13 12:21:09 -0700

    Updating saver to save all pairs

--- backtest/datasaver.py
+++ backtest/datasaver.py
@@ -1,10 +1,13 @@
+from time import sleep
+
 import requests
 import json
 from pymongo import MongoClient
 import logging
 import datasaver_config
 
-xbtusd_url = 'https://api.kraken.com/0/public/OHLC?pair=XBTUSD&interval=1'
+xbtusd_url = 'https://api.kraken.com/0/public/OHLC?pair=%s&interval=1'
+asset_pairs_url = 'https://api.kraken.com/0/public/AssetPairs'
 
 log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
@@ -17,39 +20,58 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
-response = requests.get(xbtusd_url)
-response_json = json.loads(response.text)
-ohlc = response_json['result']['XXBTZUSD']
+asset_pairs_response = requests.get(asset_pairs_url)
+ap_json = json.loads(asset_pairs_response.text)
+pairs = ap_json['result']
 
-last_time = 0
 
 client = MongoClient("mongodb://localhost:27017")
 db = client['backtest']
-cursor = db['kraken_xbtusd_1T'].find({}).sort([('time', -1)]).limit(1)
-if cursor.count() > 0:
-    last_time = cursor[0]['time']
 
 
-inserted_rows = 0
-for row in ohlc:
-    row_time, row_open, row_high, row_low, row_close, row_vwap, row_volume, row_count = row
-
-    if row_time <= last_time:
+for pair, value in pairs.items():
+    if ".d" in pair or pair == 'XXLMXXBT':
         continue
 
-    data = {
-        'time': row_time,
-        'open': row_open,
-        'high': row_high,
-        'low': row_low,
-        'close': row_close,
-        'vwap': row_vwap,
-        'volume': row_volume,
-        'count': row_count
-    }
+    logger.info('processing pair %s' % pair)
+    get_olhc_url = xbtusd_url % pair
+    response = requests.get(get_olhc_url)
+    response_json = json.loads(response.text)
+    ohlc = response_json['result'][pair]
 
-    db['kraken_xbtusd_1T'].insert(data)
+    db_pair = "kraken_%s_1T" % pair.lower()
 
-    inserted_rows = inserted_rows + 1
+    last_time = 0
+    cursor = db[db_pair].find({}).sort([('time', -1)]).limit(1)
+    if cursor.count() > 0:
+        last_time = cursor[0]['time']
 
-logger.info('inserted %s rows' % inserted_rows)
+    inserted_rows = 0
+    for row in ohlc:
+        row_time, row_open, row_high, row_low, row_close, row_vwap, row_volume, row_count = row
+
+        if row_time <= last_time:
+            continue
+
+        data = {
+            'time': row_time,
+            'open': row_open,
+            'high': row_high,
+            'low': row_low,
+            'close': row_close,
+            'vwap': row_vwap,
+            'volume': row_volume,
+            'count': row_count
+        }
+
+        db[db_pair].insert(data)
+
+        inserted_rows = inserted_rows + 1
+
+    logger.info('inserted %s rows for ticker %s' % (inserted_rows, pair))
+
+
+
+
+
+
